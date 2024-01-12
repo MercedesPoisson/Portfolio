52ac6507466fa23389f0b9d924092b1dc7e0d70e
MercedesPoisson <mercedespoisson@yahoo.com>
2024-01-12 19:15:57 -0300

    rename sobre

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import ColorContext from "./ColorContext";
 import { useTranslation }  from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope as faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
--- src/components/RightIcons.jsx
+++ src/components/RightIcons.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faFile as faSolidFile } from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope as faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 // import MercedesPoisson from "../utils/MercedesPoisson.pdf";
 
